d348df444816c73c68db32e4dc838515e2e8ce0b
pedroygor <pedroygor2010@hotmail.com>
2023-03-30 20:26:00 -0300

    feat: adiciona a implementação da função filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -64,4 +64,4 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    return [job for job in jobs if job["job_type"] == job_type]
