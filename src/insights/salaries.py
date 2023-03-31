03e51affc9c1aec3bf42eeac386db11321120caf
pedroygor <pedroygor2010@hotmail.com>
2023-03-31 14:37:27 -0300

    feat: adiciona a implementação da função filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -101,4 +101,12 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    list_of_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_of_jobs.append(job)
+        except ValueError:
+            continue
+
+    return list_of_jobs
